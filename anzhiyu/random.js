29f4b2fde3be82a5692e6160dc0ac7389644d42e
YEXIAONAN <YEXIAONANAIR@outlook.com>
2024-01-26 11:36:36 +0800

    2

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/25/hello-world/"];function toRandomPost(){
+var posts=["2024/01/26/hello-world/","2024/01/26/大数据应用与服务-阶段一-教程文档/","2024/01/26/MarkDown写法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
